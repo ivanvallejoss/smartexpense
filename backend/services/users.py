0ea91ae82a900be6459b730fffb2e8aa5b6dc1e0
ivanvallejoss <ivanvallejos06@gmail.com>
2026-01-30 20:47:40 -0300

    feature/historial added and configured

---
+++ backend/services/users.py
@@ -0,0 +1,11 @@
+from apps.core.models import User
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+async def get_user_with_telegram_id(telegram_id: int):
+    try:
+        user = User.objects.aget(telegram_id=telegram_id)
+    except User.DoesNotExist:
+        return None
