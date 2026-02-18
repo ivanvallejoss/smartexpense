2dae4a620a739f91190c5ad397cfbc0c5838199a
ivanvallejoss <ivanvallejos06@gmail.com>
2026-02-18 15:21:42 -0300

    feat(layout): add bottom navigation bar and layout wrapper for mobile UX

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,28 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import History from './pages/History';
+import MainLayout from './components/layout/MainLayout';
+import { Heading2 } from 'lucide-react';
 
 function App() {
   return (
-    <div>
-      {/* ⚠️ MENU TEMPORAL (Solo para desarrollo) */}
-      <nav style={{ padding: '10px', borderBottom: '1px solid #ccc', marginBottom: '20px' }}>
-        <Link to="/" style={{ marginRight: '10px' }}>Home</Link> | 
-        <Link to="/login" style={{ margin: '0 10px' }}>Login</Link> | 
-        <Link to="/history" style={{ marginLeft: '10px' }}>Historial</Link>
-      </nav>
+    <Routes>
+      {/* Rutas publicas (sin layout, pantalla completa) */}
+      <Route path="/login" element={<Login />} />
 
-      {/* Aquí es donde React renderiza la página activa */}
-      <Routes>
+      {/* Tuas privadas (con BottomNav) */}
+      <Route element={< MainLayout />}>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/history" element={<History />} />
-        
-        {/* Ruta 404 (Opcional por ahora) */}
-        <Route path="*" element={<h2>404 - Página no encontrada</h2>} />
-      </Routes>
-    </div>
+        {/* PlaceHolder para el perfil del usuario */}
+        <Route path="/profile" element={<h2>Perfile de Usuario</h2>} />
+      </Route>
+
+      {/* 404 */}
+      <Route path="*" element={<h2>404 = No encontrado</h2>} />
+    </Routes>
+
   );
 }
 
