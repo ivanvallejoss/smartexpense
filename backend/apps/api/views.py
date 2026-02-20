f4a5bcd8b3edd5b3af94fc6b92a626c31ae085a0
ivanvallejoss <ivanvallejos06@gmail.com>
2026-02-19 21:47:48 -0300

    changed the limit of expenses that we can send throw the endpoint

--- backend/apps/api/views.py
+++ backend/apps/api/views.py
@@ -9,7 +9,7 @@ api = NinjaAPI(title="SmartExpense API", auth=GlobalAuth())
 
 #  Ninja automaticamente toma la lista de objetos de Django y los pase por el molde de ExpenseOut
 @api.get("/expenses/", response=List[ExpenseOut])
-async def list_expenses(request, limit:int = 10):
+async def list_expenses(request, limit:int = 50):
     """
     Obtiene los ultimos 10 gastos del usuario.
     """
