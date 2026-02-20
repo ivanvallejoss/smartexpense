d1cf1601a39bc6e80ec1083cd3e4ca9a12273ded
ivanvallejoss <ivanvallejos06@gmail.com>
2026-02-20 18:40:10 -0300

    modified CORS settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -222,12 +222,6 @@ LOGGING = {
 # CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_ALL_ORIGINS = False
 
-CORS_ALLOWED_OPTIONS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173"
-]
-
-# If production environment
-FRONTEND_URL = os.environ.get('FRONTEND_URL')
-if FRONTEND_URL:
-    CORS_ALLOWED_OPTIONS = [FRONTEND_URL]
+FRONTEND_URL = os.environ.get('FRONTEND_URL', 'http://localhost:5173')
+CORS_ALLOWED_ORIGINS = [FRONTEND_URL,]
+# print(f"CORS ORIGINS PERMITIDOS: {CORS_ALLOWED_OPTIONS}")
