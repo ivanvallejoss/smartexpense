f398358dc6765948fd2267437391d7f908d9467a
ivanvallejoss <ivanvallejos06@gmail.com>
2026-02-19 21:46:57 -0300

    fix(logic): fixed fetching that was trying to reach a non existent endpoint

--- frontend/src/hooks/useDashboardData.ts
+++ frontend/src/hooks/useDashboardData.ts
@@ -12,13 +12,24 @@ export function useDashboardData() {
         const fetchData = async () => {
             try {
                 setLoading(true);
-                // Hacemos las dos peticiones en paralelo para ganar tiempo
-                const [txData, balanceData] = await Promise.all([
-                    ExpenseService.getAll(),
-                    ExpenseService.getBalance()
-                ]);
-                setExpenses(txData);
-                setBalance(balanceData);
+
+                const expensesData = await ExpenseService.getAll();
+
+                // 2. CÁLCULO MANUAL (Mitigación en Frontend)
+                // Sumamos todos los montos del array que nos devolvió el backend
+                const totalCalculado = expensesData.reduce((acumulador, gasto) => {
+                    return acumulador + gasto.amount;
+                }, 0);
+
+                // Devolvemos las expensas
+                setExpenses(expensesData);
+                
+                // Seteamos manualmente el objeto que espera recibir el balance
+                setBalance({
+                    totalSpent: totalCalculado,
+                    currency: 'ARS',
+                    trend: 0 // Dato mockeado temporalmente
+                });
             } catch(err){
                 setError('Error al cargar los datos');
                 console.error(err);
