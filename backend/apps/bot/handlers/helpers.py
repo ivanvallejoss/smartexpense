2d1c34b6c67df03017a88685f700915d681ca164
ivanvallejoss <ivanvallejos06@gmail.com>
2026-03-22 15:54:14 -0300

    deleted old unhelpful comment

--- backend/apps/bot/handlers/helpers.py
+++ backend/apps/bot/handlers/helpers.py
@@ -9,19 +9,16 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 #            KEYBOARD MARK UP
 # ---------------------------------------
 
-# =======================================================================================
-def get_delete_keyboard_markup(expense_id):                                                    #|
-    """                                                                                 #|
-    Helper to orchestrate the inline buttons for messages                               #|
-    """                                                                                 #| NEEDS TO BE RENAMED TO: get_delete_keyboard_markup
-                                                                                        #| didn't do it yet because I need to check where it affect
-    keyboard = [                                                                        #|
-            [InlineKeyboardButton("Eliminar", callback_data=f"del:{expense_id}"),]      #|
-        ]                                                                               #|
-    markup = InlineKeyboardMarkup(keyboard)                                             #|
-                                                                                        #|
-    return markup                                                                       #|
-# =======================================================================================
+def get_delete_keyboard_markup(expense_id):                                             
+    """                                                                                 
+    Helper to orchestrate the inline buttons for messages                               
+    """                                                                                                                                                                         
+    keyboard = [                                                                        
+            [InlineKeyboardButton("Eliminar", callback_data=f"del:{expense_id}"),]      
+        ]                                                                               
+    markup = InlineKeyboardMarkup(keyboard)                                             
+                                                                                        
+    return markup                                                                       
 
 
 
